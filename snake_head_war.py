c96ce4c29a82fb3871bcd20c4a4bcb63c91a5f67
RobinKumar5986 <72180986+RobinKumar5986@users.noreply.github.com>
2020-10-04 09:38:52 +0530

    Update snake_head_war.py

--- snake_head_war.py
+++ snake_head_war.py
@@ -1,5 +1,5 @@
 # python-snake-head-war
-it is created with python and the main module which is been used in this program is python
+#it is created with python and the main module which is been used in this program is python
 #its a snake head racing game
 #modules which are imported 
 import turtle
